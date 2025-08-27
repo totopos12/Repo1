672f76910f2a6085e6905c36454d9f6e98ac58b9
totopos12 <ricardovazquez110302@gmail.com>
2025-08-27 13:09:44 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+const Home = () => {
+return(
+  <body>
+  <header className="Fisrtclass">
+    <p>s</p>
+    FAN CONTROLS
+    </header>
+    <footer>CETYS</footer>
+  </body> 
+)
+}
+
+export default Home
+
+  
