2f39d6fec5e2f988a6b08c67e7851ff14dba7699
RicardoVazquez <ricardovazquez110302@gmail.com>
2025-08-27 19:32:51 -0700

    botones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import "./globals.css";
+import "./styles/globals.css";
+import Button1, { Button3, Button2 } from "./Components/Botones/button";
+import Fan_Logo from "./assets/img/Fan_Logo.png";
 
 const Home = () => {
 return(
   <body>
-  <header className="Fisrtclass">
-    <p>s</p>
+  <header className="Firstclass">
+    <img src={Fan_Logo.src} height={100} alt="img1"/>
     FAN CONTROLS
     </header>
     <footer>CETYS</footer>
+    <Button1></Button1>
+    <Button2></Button2>
+    <Button3></Button3>
   </body> 
 )
 }
